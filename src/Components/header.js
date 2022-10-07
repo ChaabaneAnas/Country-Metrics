80a63d3e0de6993ab2bc5a5dd829f0e8bcf78b04
Anas <anas.chaabane98@gmail.com>
2022-10-07 16:47:26 +0100

    fix header color

--- src/Components/header.js
+++ src/Components/header.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <Link to="/"><AiOutlineArrowLeft /></Link>
-    <h2> Country Metrics App</h2>
+    <h2> Country Metrics</h2>
     <BiMicrophone />
     <AiTwotoneSetting />
   </header>
